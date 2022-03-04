82b4cd30a6e76cfab3bfd412a3e6ee5cdd970289
Grosse Riese <plmman2010@gmail.com>
2022-03-04 10:25:13 +0100

    change
    hej

--- Lab_2/Q_2.py
+++ Lab_2/Q_2.py
@@ -29,6 +29,14 @@ v6 = pie5
 v7 = pie5[450099:453102]
 v8 = pie5[710099:710108]
 
+"""
+Vi tittar p ̊a tv ̊a p ̊a varande f ̈oljande tal vi−1, vi. Om vi−1 = vi  ̈ar de en del av en sekvens av
+likandana siffror, om d ̈aremot vi−1 6= vi  ̈ar vi−1 sista siffran i en sekvens och vi  ̈ar f ̈orsta siffran
+i en ny sekvens av likadana siffror.
+Vi anv ̈ander variabeln lgd f ̈or att ber ̈akna l ̈angden av alla delsekvenser av likadana siffror, s ̊a
+om vi−1 = vi  ̈okar vi lgd med 1. Om vi−1 6= vi har en ny delsekvens p ̊ab ̈orjats och vi l ̊ater lgd
+bli 1.
+"""
 def langsta_sekvensen(arr):
     
     lgd = 1
@@ -39,7 +47,7 @@ def langsta_sekvensen(arr):
         print("An empty array is not allowed!")
         return 0
     elif len(arr) == 1:
-        print(f"Det blir en enda siffra: {arr[0]} ,och har {element} sekvens")
+        print(f'Det blir en enda siffra: {arr[0]} ,och har {element} sekvens')
         return 0
     
     for i in np.arange(1,len(arr)): 
--- Lab_2/Q_3.py
+++ Lab_2/Q_3.py
@@ -6,20 +6,25 @@ Created on Mon Jan 31 23:24:18 2022
 """
 import numpy as np 
 
+class my_dictionary(dict): 
+    # __init__ function 
+    def __init__(self): 
+        self = dict() 
+        
+    # Function to add key:value 
+    def add(self, key, value): 
+        self[key] = value 
+
 tal = np.array([1, 2, 2, 1, 1, 1, 4, 4, 4, 4, 5, 5, 5, 1, 7, 9])
 
 def antalet_sekvenser(arr):
-    
+    dict_obj = my_dictionary() 
     count = {} #dictionary
-    
-    for i in arr: 
-        if not i in count:
-            count[i] = 0
-        else:
-            count[i] += 1
+    countNum = 0
+    for i in np.arange(len(arr)):
     
     for i,(k,v) in enumerate(count.items()):#key, value loop
         print(f"Array har {v} sekvenser av längd {k}")
     #print(f"\n  {count} ")
-   
+    print(f"Longest: {000} ")
 antalet_sekvenser(tal)
